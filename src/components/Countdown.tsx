bd545ee12a990deffdf0591c0a3fb69d5be44ea8
brenosekiro <b88660501@gmail.com>
2021-02-23 10:22:31 -0300

    time

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,17 +1,46 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/components/Countdown.module.css'
 
 export default function Countdown() {
+
+    const [time, setTime] = useState(25 * 60)
+    const [active, setActive] = useState(false)
+
+    useEffect(() => {
+        if(active && time > 0) {
+            setTimeout(() => {
+                setTime(time - 1)
+            }, 1000)
+        }
+    }, [active, time])
+
+    const minutes = Math.floor(time / 60)
+    const seconds = time % 60
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
+
+    function startCountDown() {
+        setActive(true)
+    }
+
     return (
-        <div className={styles.container}>
-            <div>
-                <span>2</span>
-                <span>5</span>
+        <div>
+            <div className={styles.container}>
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
+                </div>
+                <div>:</div>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
             </div>
-            <div>:</div>
-            <div>
-                <span>2</span>
-                <span>5</span>
-            </div>
+
+            <button type="button" onClick={startCountDown} className={styles.start}>
+                Iniciar um ciclo
+            </button>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,16 @@ import Countdown from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
 import styles from '../styles/pages/Home.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <Head>
+        <title>Inicio | Move it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
